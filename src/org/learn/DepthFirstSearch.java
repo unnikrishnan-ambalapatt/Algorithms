c206a147f9cfadf7ef828d9920a1cf6bf17923bb
Unnikrishnan Ambalapatt <49778290+unnikrishnan-ambalapatt@users.noreply.github.com>
2020-06-10 16:37:30 -0700

    Added test cases for DFS

--- src/org/learn/DepthFirstSearch.java
+++ src/org/learn/DepthFirstSearch.java
@@ -1,8 +1,6 @@
 package org.learn;
 
-import java.util.Deque;
-import java.util.LinkedList;
-import java.util.List;
+import java.util.*;
 
 public class DepthFirstSearch {
 
@@ -10,18 +8,39 @@ public class DepthFirstSearch {
         Deque<Node> stack = new LinkedList();
         stack.push(root);
         Node current = null;
-        while(!stack.isEmpty()) {
+        while (!stack.isEmpty()) {
             current = stack.pop();
-            if(!current.isVisited()) {
+            if (!current.isVisited()) {
                 current.setVisited(true);
-                for(Node child : current.getChildren()) {
-                    stack.push(child);
+                current.print();
+                if(null != current.getChildren()) {
+                    for (Node child : current.getChildren()) {
+                        stack.push(child);
+                    }
                 }
             }
         }
     }
 
     public static void main(String[] args) {
+        int counter = 1;
+        Node root = new Node(1);
+        Node node11 = new Node(2);
+        Node node12 = new Node(3);
+        List<Node> children1 = Arrays.asList(node11, node12);
+        root.setChildren(children1);
+
+        Node node111 = new Node(4);
+        Node node112 = new Node(5);
+        List<Node> children11 = Arrays.asList(node111, node112);
+        node11.setChildren(children11);
+
+        Node node211 = new Node(6);
+        Node node212 = new Node(7);
+        List<Node> children12 = Arrays.asList(node211, node212);
+        node12.setChildren(children12);
+
+        depthFirstSearch(root);
 
     }
 }
@@ -58,5 +77,9 @@ class Node {
 
     public void setChildren(List<Node> children) {
         this.children = children;
+    }
+
+    public void print() {
+        System.out.println("[" + this.value + "]");
     }
 }
